9502f85e7c541ad504c4282ef46eb6550bb0fecc
Hasnain Mucklai <mucklaihasnain@gmail.com>
2025-12-04 19:07:07 -0800

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import PersonList from './components/PersonList.js';
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <h1 className="title">IAM Innovators Team 3</h1>
+        <div className="title-decoration"></div>
+      </header>
       <PersonList/>
     </div>
   )
--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -14,16 +14,43 @@ export default class PersonList extends React.Component {
       })
   }
 
+  getInitials(name) {
+    if (!name) return '?';
+    return name.split(' ').map(n => n[0]).join('').substring(0, 2);
+  }
+
   render() {
+    const { persons } = this.state;
+
+    if (persons.length === 0) {
+      return (
+        <div className="person-list-container">
+          <div className="loading-container">
+            <div className="loading-spinner"></div>
+            <p className="loading-text">Loading team members...</p>
+          </div>
+        </div>
+      );
+    }
+
     return (
-      <ul>
-        {
-          this.state.persons
-            .map(person =>
-              <li key={person.id}>{person.id}{person.name}</li>
-            )
-        }
-      </ul>
-    )
+      <div className="person-list-container">
+        <ul className="person-grid">
+          {persons.map(person => (
+            <li key={person.id} className="person-card">
+              <div className="card-content">
+                <div className="person-avatar">
+                  <span className="avatar-text">{this.getInitials(person.name)}</span>
+                </div>
+                <div className="person-info">
+                  <span className="person-id">ID: {person.id}</span>
+                  <h3 className="person-name">{person.name}</h3>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
